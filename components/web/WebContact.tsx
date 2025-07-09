7cb9f9fff221822b9da21561d0f3c7213cf8eb6d
Hakan <86249094+HDemir23@users.noreply.github.com>
2025-07-09 13:37:12 +0300

    babel

--- components/web/WebContact.tsx
+++ components/web/WebContact.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { View } from "react-native";
 import Divider from "../Divider/divider";
+import Button from "./Buttons/Button";
 import Contact from "./ContactIcons/Contact";
 import Mail from "./Mail/Mail";
 
 export default function WebContact() {
   return (
     <View>
+      <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+        <Button text="Resume" onPress={() => console.log("clicked")} />
+        <Button text="Mobile App" onPress={() => console.log("clicked")} />
+      </View>
+      <Divider />
       <Contact />
       <Divider />
       <Mail />
