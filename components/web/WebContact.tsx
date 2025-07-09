de5bb483798bc0c30a89c8eac9074b00c965b26f
Hakan <86249094+HDemir23@users.noreply.github.com>
2025-07-09 13:42:53 +0300

    resume and buttons added

--- components/web/WebContact.tsx
+++ components/web/WebContact.tsx
@@ -6,10 +6,12 @@ import Contact from "./ContactIcons/Contact";
 import Mail from "./Mail/Mail";
 
 export default function WebContact() {
+  const resume =
+    "https://docs.google.com/document/d/1ZKeHLH1XcS_kLFjsHAbqszWEeG7JD39OpQYDPyO1his/export?format=pdf";
   return (
     <View>
       <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
-        <Button text="Resume" onPress={() => console.log("clicked")} />
+        <Button text="Resume" onPress={() => open(resume)} />
         <Button text="Mobile App" onPress={() => console.log("clicked")} />
       </View>
       <Divider />
